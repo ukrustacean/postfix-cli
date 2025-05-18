0912532e5c2e217bad7295a7b6dc768356141c4b
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2025-05-18 12:59:10 +0200

    Implement `ComputeHandler`

--- handler.go
+++ handler.go
@@ -1,12 +1,49 @@
 package postfixcli
 
+import (
+	"bufio"
+	"io"
+	"strconv"
+	"strings"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
+	reader := bufio.NewReader(ch.Input)
+	expr, err := reader.ReadString('\n')
+
+	expr = strings.TrimSpace(expr)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := EvaluatePostfix(expr)
+
+	if err != nil {
+		return err
+	}
+
+	s := strconv.Itoa(result)
+
+	writer := bufio.NewWriter(ch.Output)
+	_, err = writer.WriteString(s + "\n")
+
+	if err != nil {
+		return err
+	}
+
+	err = writer.Flush()
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
